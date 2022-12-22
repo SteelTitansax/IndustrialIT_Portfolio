46e6bfd48c296be674e8fc4bf5d28ad9c51bd883
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2022-12-22 20:09:56 +0100

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,20 +1,46 @@
 
 export const skills = [ 
     {
-        skill: 'Lorem Ipsum',
-        degree : 'Lorem Ipsum',
-        project : '/'
+        skill: 'BH Chemical engineering',
+        degree : 'Universitat Autonoma de barcelona',
+        project : 'https://www.dropbox.com/sh/0hphyx8llr9bvu7/AAArof83CAi9Awe9PvemWEGca?dl=0'
+    },
+    {
+        skill: 'Healt and safety Master',
+        degree : 'Universidad Ceu San Pablo',
+        project : 'https://www.dropbox.com/sh/to5o9owoelv300d/AADpnXG5E0edLunAg5iJc016a?dl=0'
+
+    },
+    {
+        skill: 'HNC Web Development',
+        degree : 'Glasgow Kelvin College',
+        project : 'https://www.dropbox.com/sh/kwkekm9xnpf4h86/AADv7DwZlcdxklb88r3CVG-la?dl=0'
+    }, {
+        skill: 'Bachelor Computer Science',
+        degree : 'Institut Obert de Cataluña',
+        project : 'https://www.dropbox.com/sh/41vx5o9ombbl83k/AADccV-jeM65XVwawhrFVBOBa?dl=0'
+    }, {
+        skill: 'Machine Learning Certificate',
+        degree : 'Fondos de recuperacion europea',
+        project : ''
+
     }
 ]
 
 
 export const experiences = [
     {
-        title: "Lorem Ipsum",
-        description : "Lorem Ipsum",
+        title: 'RPA developer at OMYA - Barcelona/Spain',
+        description : "RPA developing using UiPath, PowerAutomate and Azure Devops. Power Platform developer using PowerApps, PowerAutomate and Office365 services.",
         from: "Jun 2022",
         to:"present",
-        url:"/"
+        url:"https://www.omya.com/"
+    },{
+        title: 'Team Leader Operator in Bridge of Weir',
+        description : "Leading a line team of 5 people in Leather treatment operation process.",
+        from: "Sept 2019",
+        to:"Sept 2016",
+        url:"https://www.bridgeofweirleather.com/"
     }
    
 ]
@@ -39,9 +65,15 @@ export const BlogArticles= [
 
 export const projects = [ 
     {
-        name: "Lorem Ipsum",
-        description:"Lorem Ipsum",
-        image:"fullstack.jpg",
+        name: "Industrial Applications",
+        description:"Please check the industrial applications section of this PortFolio",
+        image:"canvas.png",
+        url:"/",
+    },
+    {
+        name: "Industrial Simulations",
+        description:"Please check the industrial simulations section of this PortFolio",
+        image:"azure.png",
         url:"/",
     }
 ]
